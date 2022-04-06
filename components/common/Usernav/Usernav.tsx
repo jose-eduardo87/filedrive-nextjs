12491282442828753422992cd717fa83c5b8d5d2
José Eduardo <eduardoaraujodev@gmail.com>
2022-04-05 22:47:03 -0300

    Session now returns user ID.

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -18,16 +18,17 @@ const Usernav: FC = () => {
   const { data: session, status } = useSession();
   const { t } = useTranslation("common");
   const { isDark } = useTheme();
-  const isAuthenticated = status === 'authenticated';
+  const isAuthenticated = status === "authenticated";
 
-    return (
-      <div
-        className={styles.usernav}
-        style={{ backgroundColor: isDark ? "#272640" : "#FFFFFF" }}
-      >
-        <div className={styles.upperGroup}>
-          <div className={styles.profileImg}>
-            {isAuthenticated && <Image
+  return (
+    <div
+      className={styles.usernav}
+      style={{ backgroundColor: isDark ? "#272640" : "#FFFFFF" }}
+    >
+      <div className={styles.upperGroup}>
+        <div className={styles.profileImg}>
+          {isAuthenticated && (
+            <Image
               alt={`${session?.user?.name} profile image`}
               width={150}
               height={150}
@@ -35,54 +36,61 @@ const Usernav: FC = () => {
               quality={60}
               objectFit="cover"
               src={session!.user!.image || DEFAULT_AVATAR}
-            />}
-          </div>
-          <small>{isAuthenticated ? session!.user!.name : <em>Loading...</em>}</small>
-          <nav>
-            <ul className={styles.navLinks}>
-              <Link passHref href="/">
-                <li>
-                  <span className={styles.icon}>
-                    <Home {...getIconStyles(isDark)} />
-                  </span>
-                  Home
-                </li>
-              </Link>
-              <Link passHref href="/drive">
-                <li>
-                  <span className={styles.icon}>
-                    <Dashboard {...getIconStyles(isDark)} />
-                  </span>
-                  Dashboard
-                </li>
-              </Link>
-              <Link passHref href="/drive/files">
-                <li>
-                  <span className={styles.icon}>
-                    <Filedrive {...getIconStyles(isDark)} />
-                  </span>
-                  {t("usernav-file-manager")}
-                </li>
-              </Link>
-              <Link passHref href="/drive/settings">
-                <li>
-                  <span className={styles.icon}>
-                    <Gear {...getIconStyles(isDark)} />
-                  </span>
-                  {t("usernav-settings")}
-                </li>
-              </Link>
-            </ul>
-          </nav>
+              priority
+            />
+          )}
         </div>
-          <p className={styles.logout} onClick={() => signOut({ callbackUrl: "/" })}>
-            <span style={{ marginRight: ".5rem", verticalAlign: "middle" }}>
-              <Logout width={16} fill="red" />
-            </span>
-            {t("usernav-logout")}
-          </p>
+        <small>
+          {isAuthenticated ? session!.user!.name : <em>Loading...</em>}
+        </small>
+        <nav>
+          <ul className={styles.navLinks}>
+            <Link passHref href="/">
+              <li>
+                <span className={styles.icon}>
+                  <Home {...getIconStyles(isDark)} />
+                </span>
+                Home
+              </li>
+            </Link>
+            <Link passHref href="/drive">
+              <li>
+                <span className={styles.icon}>
+                  <Dashboard {...getIconStyles(isDark)} />
+                </span>
+                Dashboard
+              </li>
+            </Link>
+            <Link passHref href="/drive/files">
+              <li>
+                <span className={styles.icon}>
+                  <Filedrive {...getIconStyles(isDark)} />
+                </span>
+                {t("usernav-file-manager")}
+              </li>
+            </Link>
+            <Link passHref href="/drive/settings">
+              <li>
+                <span className={styles.icon}>
+                  <Gear {...getIconStyles(isDark)} />
+                </span>
+                {t("usernav-settings")}
+              </li>
+            </Link>
+          </ul>
+        </nav>
       </div>
-    );
+      <p
+        className={styles.logout}
+        onClick={() => signOut({ callbackUrl: "/" })}
+      >
+        <span style={{ marginRight: ".5rem", verticalAlign: "middle" }}>
+          <Logout width={16} fill="red" />
+        </span>
+        {t("usernav-logout")}
+      </p>
+    </div>
+  );
 };
 
 export default Usernav;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import prisma from "lib/prisma";
 import type { NextApiHandler } from 'next';
+import { Session, User } from "next-auth";
 
 const options = {
   adapter: PrismaAdapter(prisma),
@@ -16,9 +19,21 @@ const options = {
           access_type: "offline",
           response_type: "code"
         }
-      }
-    })
+      },
+    }),
+    // CredentialsProvider({
+    //   async authorize(credentials, req) {
+    //     console.log(credentials);
+    //   }
+    // })
   ],
+  callbacks: {
+    async session({ session, user }: { session: Session, user: User }) {
+      session.user.id = user.id;
+
+      return session;
+    }
+  }
 };
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
