e9f09073f57f671944d2c1559011e621e9efd037
José Eduardo <eduardoaraujodev@gmail.com>
2022-06-14 15:15:01 -0300

    Removed two unused models - Account and Session

--- models/Account.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from "@/lib/index";
-
-const Account = () => Object.assign({}, prisma.account);
-
-export default Account();
--- models/Session.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from "@/lib/index";
-
-const Session = () => Object.assign({}, prisma.session);
-
-export default Session();
